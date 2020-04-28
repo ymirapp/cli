d2c4e1787ee08be99c6453ee92ff32a44adb8f3c
Carl Alexander <carlalexander@gmail.com>
2020-04-28 17:34:48 -0400

    feat: delete hidden directory once artifact uploaded

---
+++ src/Deployment/CleanupStep.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ymir command-line tool.
+ *
+ * (c) Carl Alexander <support@ymirapp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ymir\Cli\Deployment;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Ymir\Cli\Console\OutputStyle;
+
+class CleanupStep implements DeploymentStepInterface
+{
+    /**
+     * The file system.
+     *
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * The hidden directory used by Ymir.
+     *
+     * @var string
+     */
+    private $hiddenDirectory;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(Filesystem $filesystem, string $hiddenDirectory)
+    {
+        $this->filesystem = $filesystem;
+        $this->hiddenDirectory = rtrim($hiddenDirectory, '/');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function perform(int $deploymentId, OutputStyle $output)
+    {
+        $output->info('Cleaning up deployment files');
+
+        $this->filesystem->remove($this->hiddenDirectory);
+    }
+}
