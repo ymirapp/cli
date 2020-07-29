44b727a9b8ebbe72994f1bddea039d0be967f202
Carl Alexander <carlalexander@gmail.com>
2020-07-29 17:17:02 -0400

    feat(command): added `php:info` command

---
+++ src/Command/Php/PhpInfoCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ymir command-line tool.
+ *
+ * (c) Carl Alexander <support@ymirapp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ymir\Cli\Command\Php;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Ymir\Cli\Command\AbstractInvocationCommand;
+use Ymir\Cli\Console\OutputStyle;
+
+class PhpInfoCommand extends AbstractInvocationCommand
+{
+    /**
+     * The name of the command.
+     *
+     * @var string
+     */
+    public const NAME = 'php:info';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('Get information about PHP on the cloud provider')
+            ->addOption('environment', null, InputOption::VALUE_REQUIRED, 'The environment name', 'staging');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function perform(InputInterface $input, OutputStyle $output)
+    {
+        $environment = (string) $this->getStringOption($input, 'environment');
+
+        $output->info(sprintf('Get information about PHP from the "<comment>%s</comment>" environment', $environment));
+
+        $result = $this->invokeEnvironmentFunction($environment, [
+            'php' => '--info',
+        ]);
+
+        $output->newLine();
+        $output->write("${result['output']}");
+
+        return $result['exitCode'];
+    }
+}
