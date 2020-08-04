7ab8765f7239ff9274153ae7ce7144190054d068
Carl Alexander <carlalexander@gmail.com>
2020-08-04 12:35:01 -0400

    fix: don't activate plugin if it's already active
    This breaks the media library uploader on multisite due to some crazy
    edge case.

--- stubs/activate-ymir-plugin.php
+++ stubs/activate-ymir-plugin.php
@@ -7,9 +7,16 @@ function activate_ymir_plugin() {
         require_once ABSPATH . 'wp-admin/includes/plugin.php';
     }
 
+    // Important to not activate the plugin if it's already active. It's fine for normal sites, but
+    // it breaks the backbone media uploader with multisite due to a weird edgecase with the "networkwide"
+    // query value being set globally.
     foreach (get_plugins() as $file => $plugin) {
-        if (preg_match('/ymir\.php$/', $file)) {
-            activate_plugin($file, '', defined('MULTISITE') ? MULTISITE : false);
+        if (1 !== preg_match('/ymir\.php$/', $file)) {
+            continue;
+        } elseif (defined('MULTISITE') && MULTISITE && !is_plugin_active_for_network($file)) {
+            activate_plugin($file, '', true);
+        } elseif ((!defined('MULTISITE') || !MULTISITE) && !is_plugin_active($file)) {
+            activate_plugin($file);
         }
     }
 }
