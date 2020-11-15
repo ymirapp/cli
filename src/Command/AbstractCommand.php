85c07dc1befb651e9614b348389a584f830a0bd3
Carl Alexander <carlalexander@gmail.com>
2020-11-15 15:24:44 -0500

    fix: fix broken condition

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -138,7 +138,7 @@ abstract class AbstractCommand extends Command
 
         if (null === $value) {
             return $value;
-        } elseif (is_array($value) || is_numeric($value)) {
+        } elseif (is_array($value) || !is_numeric($value)) {
             throw new InvalidArgumentException(sprintf('The "--%s" option must be a numeric value', $option));
         }
 
