042d775a7c727c91fd8c15ab04f9e810e54863e6
Carl Alexander <carlalexander@gmail.com>
2020-12-02 02:58:44 -0500

    feat: rework `install-plugin` command to use github repository

--- src/Command/InstallPluginCommand.php
+++ src/Command/InstallPluginCommand.php
@@ -13,10 +13,17 @@ declare(strict_types=1);
 
 namespace Ymir\Cli\Command;
 
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Process\Process;
+use Tightenco\Collect\Support\Arr;
+use Ymir\Cli\ApiClient;
+use Ymir\Cli\CliConfiguration;
 use Ymir\Cli\Console\ConsoleOutput;
+use Ymir\Cli\ProjectConfiguration;
 
 class InstallPluginCommand extends AbstractProjectCommand
 {
@@ -26,6 +33,31 @@ class InstallPluginCommand extends AbstractProjectCommand
      * @var string
      */
     public const NAME = 'install-plugin';
+
+    /**
+     * The file system.
+     *
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * The project directory where we want to install the plugin.
+     *
+     * @var string
+     */
+    private $projectDirectory;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(ApiClient $apiClient, CliConfiguration $cliConfiguration, Filesystem $filesystem, ProjectConfiguration $projectConfiguration, string $projectDirectory)
+    {
+        parent::__construct($apiClient, $cliConfiguration, $projectConfiguration);
+
+        $this->filesystem = $filesystem;
+        $this->projectDirectory = rtrim($projectDirectory, '/');
+    }
 
     /**
      * {@inheritdoc}
@@ -42,24 +74,76 @@ class InstallPluginCommand extends AbstractProjectCommand
      */
     protected function perform(InputInterface $input, ConsoleOutput $output)
     {
-        $command = null;
-        $message = 'Installing Ymir plugin ';
-        $projectType = $this->projectConfiguration->getProjectType();
+        $message = 'Installing Ymir plugin';
+        $projectType = strtolower($this->projectConfiguration->getProjectType());
+
+        if (!in_array($projectType, ['bedrock', 'wordpress'])) {
+            throw new RuntimeException('Can only install plugin for "bedrock" and "wordpress" projects');
+        }
 
         if ('bedrock' === $projectType) {
-            $command = 'composer require ymirapp/wordpress-plugin';
-            $message .= 'using Composer';
+            $output->info($message.' using Composer');
+            $this->installUsingComposer();
         } elseif ('wordpress' === $projectType) {
-            $command = 'cp -R /Users/carlalexander/Projects/ymir/wordpress-plugin wp-content/plugins/ymir';
-            $message .= 'manually';
+            $output->info($message.' from GitHub');
+            $this->installFromGitHub();
+        }
+    }
+
+    /**
+     * Install the WordPress plugin by downloading it from GitHub.
+     */
+    private function installFromGitHub()
+    {
+        $client = new Client();
+        $response = $client->request('GET', 'https://api.github.com/repos/ymirapp/wordpress-plugin/tags');
+
+        if (200 !== $response->getStatusCode()) {
+            throw new RuntimeException('Unable to get the latest WordPress plugin versions from the GitHub API');
         }
 
-        if (!is_string($command)) {
-            throw new RuntimeException('Unable to install Ymir plugin');
+        $latestTag = collect(json_decode((string) $response->getBody(), true))->first();
+
+        if (empty($latestTag['zipball_url'])) {
+            throw new RuntimeException('Unable to parse the WordPress plugin versions from the GitHub API');
         }
 
-        $output->info($message);
-        $process = Process::fromShellCommandline($command);
+        $downloadedZipFile = tmpfile();
+
+        if (!is_resource($downloadedZipFile)) {
+            throw new RuntimeException('Unable to open a temporary file');
+        }
+
+        fwrite($downloadedZipFile, (string) $client->request('GET', $latestTag['zipball_url'])->getBody());
+
+        $downloadedZipArchive = new \ZipArchive();
+
+        if (true !== $downloadedZipArchive->open(stream_get_meta_data($downloadedZipFile)['uri'])) {
+            throw new RuntimeException('Unable to open the WordPress plugin Zip archive from GitHub');
+        }
+
+        $pluginsDirectory = $this->projectDirectory.'/wp-content/plugins';
+        $downloadedZipArchive->extractTo($pluginsDirectory);
+
+        $files = Finder::create()
+            ->directories()
+            ->in($pluginsDirectory)
+            ->path('/^ymirapp-wordpress-plugin-/')
+            ->depth('== 0');
+
+        if (1 !== count($files)) {
+            throw new RuntimeException('Unable to find the extracted WordPress plugin');
+        }
+
+        $this->filesystem->rename($pluginsDirectory.'/'.Arr::first($files)->getFilename(), $pluginsDirectory.'/ymir-wordpress-plugin', true);
+    }
+
+    /**
+     * Install the WordPress plugin using composer.
+     */
+    private function installUsingComposer()
+    {
+        $process = Process::fromShellCommandline('composer require ymirapp/wordpress-plugin');
         $process->run();
 
         if (!$process->isSuccessful()) {
