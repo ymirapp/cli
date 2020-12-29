dd3e0db094f4c43c19ce52db72992aa98531c6ca
Carl Alexander <carlalexander@gmail.com>
2020-12-29 14:40:59 -0500

    chore: bump version

--- src/Application.php
+++ src/Application.php
@@ -24,7 +24,7 @@ class Application extends BaseApplication
      */
     public function __construct(iterable $commands = [])
     {
-        parent::__construct('Ymir', '1.4.1');
+        parent::__construct('Ymir', '1.4.2');
 
         foreach ($commands as $command) {
             $this->add($command);
