3824475aa6cbb81c1f30214a46c26936d1f3b604
Carl Alexander <carlalexander@gmail.com>
2021-12-28 10:37:35 -0500

    feat: add `project:list` command

---
+++ src/Command/Project/ListProjectsCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ymir command-line tool.
+ *
+ * (c) Carl Alexander <support@ymirapp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ymir\Cli\Command\Project;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Ymir\Cli\Command\AbstractCommand;
+use Ymir\Cli\Console\ConsoleOutput;
+
+class ListProjectsCommand extends AbstractCommand
+{
+    /**
+     * The name of the command.
+     *
+     * @var string
+     */
+    public const NAME = 'project:list';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('List the networks that belong to the currently active team');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function perform(InputInterface $input, ConsoleOutput $output)
+    {
+        $projects = $this->apiClient->getTeamProjects($this->cliConfiguration->getActiveTeamId());
+
+        $output->table(
+            ['Id', 'Name', 'Provider', 'Region'],
+            $projects->map(function (array $project) {
+                return [$project['id'], $project['name'], $project['provider']['name'], $project['region']];
+            })->all()
+        );
+    }
+}
