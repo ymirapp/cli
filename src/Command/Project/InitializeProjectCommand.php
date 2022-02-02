228a3bd65bbdff0d07ddfa75bc5826419721dee5
Carl Alexander <carlalexander@gmail.com>
2022-02-02 11:01:26 -0500

    fix: forgot to change `database` argument to `--server` option

--- src/Command/Project/InitializeProjectCommand.php
+++ src/Command/Project/InitializeProjectCommand.php
@@ -181,7 +181,7 @@ class InitializeProjectCommand extends AbstractCommand
                 return;
             }
 
-            $this->invoke((new NullOutput()), CreateDatabaseCommand::NAME, ['database' => Arr::get($options, 'database.server'), 'name' => Arr::get($options, 'database.name')]);
+            $this->invoke((new NullOutput()), CreateDatabaseCommand::NAME, ['name' => Arr::get($options, 'database.name'), '--server' => Arr::get($options, 'database.server')]);
         })->map(function (array $options) {
             if (empty($options)) {
                 $options = null;
