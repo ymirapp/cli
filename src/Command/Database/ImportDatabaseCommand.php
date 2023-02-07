bcdee91ea46d0a7638ff27ea3982b91666287e9d
Carl Alexander <carlalexander@gmail.com>
2023-02-07 12:21:54 -0500

    feat: add option to force database import and ignore sql errors

--- src/Command/Database/ImportDatabaseCommand.php
+++ src/Command/Database/ImportDatabaseCommand.php
@@ -62,7 +62,8 @@ class ImportDatabaseCommand extends AbstractDatabaseCommand
             ->addArgument('name', InputArgument::OPTIONAL, 'The name of the database to import')
             ->addOption('server', null, InputOption::VALUE_REQUIRED, 'The ID or name of the database server to import a database to')
             ->addOption('user', null, InputOption::VALUE_REQUIRED, 'The user used to connect to the database server')
-            ->addOption('password', null, InputOption::VALUE_REQUIRED, 'The password of the user connecting to the database server');
+            ->addOption('password', null, InputOption::VALUE_REQUIRED, 'The password of the user connecting to the database server')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force the import even if there are SQL errors');
     }
 
     /**
@@ -97,7 +98,7 @@ class ImportDatabaseCommand extends AbstractDatabaseCommand
 
         $output->infoWithDelayWarning(sprintf('Importing "<comment>%s</comment>" to the "<comment>%s</comment>" database', $file, $name));
 
-        $command = sprintf('%s %s | mysql --protocol=TCP --host=%s --port=%s --user=%s --password=%s %s', str_ends_with($file, '.sql.gz') ? 'gunzip <' : 'cat', $file, $host, $port, $user, $password, $name);
+        $command = sprintf('%s %s | mysql %s --protocol=TCP --host=%s --port=%s --user=%s --password=%s %s', str_ends_with($file, '.sql.gz') ? 'gunzip <' : 'cat', $file, $this->getBooleanOption($input, 'force') ? '--force' : '', $host, $port, $user, $password, $name);
 
         Process::runShellCommandline($command, null, null);
 
