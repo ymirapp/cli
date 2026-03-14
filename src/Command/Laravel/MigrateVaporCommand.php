5767f53437338c65ac008ff5956ac93e639c5aef
Carl Alexander <carlalexander@gmail.com>
2026-03-14 12:15:19 -0400

    feat: add `laravel:vapor:migrate` command

---
+++ src/Command/Laravel/MigrateVaporCommand.php
@@ -0,0 +1,267 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ymir command-line tool.
+ *
+ * (c) Carl Alexander <support@ymirapp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ymir\Cli\Command\Laravel;
+
+use Illuminate\Support\Collection;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+use Ymir\Cli\ApiClient;
+use Ymir\Cli\Command\AbstractCommand;
+use Ymir\Cli\Command\LocalProjectCommandInterface;
+use Ymir\Cli\Dockerfile;
+use Ymir\Cli\Exception\InvalidInputException;
+use Ymir\Cli\Exception\Project\UnsupportedProjectException;
+use Ymir\Cli\ExecutionContextFactory;
+use Ymir\Cli\Project\Configuration\Laravel\VaporConfigurationChange;
+use Ymir\Cli\Project\EnvironmentConfiguration;
+use Ymir\Cli\Project\Type\LaravelProjectType;
+use Ymir\Cli\Support\Arr;
+
+class MigrateVaporCommand extends AbstractCommand implements LocalProjectCommandInterface
+{
+    /**
+     * The name of the command.
+     *
+     * @var string
+     */
+    public const NAME = 'laravel:vapor:migrate';
+
+    /**
+     * The project Dockerfile service.
+     *
+     * @var Dockerfile
+     */
+    private $dockerfile;
+
+    /**
+     * The file system.
+     *
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(ApiClient $apiClient, ExecutionContextFactory $contextFactory, Dockerfile $dockerfile, Filesystem $filesystem)
+    {
+        parent::__construct($apiClient, $contextFactory);
+
+        $this->dockerfile = $dockerfile;
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('Migrate vapor.yml environment configuration into ymir.yml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function perform()
+    {
+        if (!$this->getProjectConfiguration()->getProjectType() instanceof LaravelProjectType) {
+            throw new UnsupportedProjectException('You can only use this command with Laravel projects');
+        }
+
+        $vaporConfiguration = $this->getVaporConfiguration();
+        $vaporEnvironments = Arr::get($vaporConfiguration, 'environments');
+
+        if (!is_array($vaporEnvironments)) {
+            throw new InvalidInputException('No valid "environments" key found in vapor.yml file');
+        }
+
+        $matchedEnvironments = $this->getProjectConfiguration()->getEnvironments()->keys()->intersect(collect($vaporEnvironments)->keys())->values();
+
+        if ($matchedEnvironments->isEmpty()) {
+            $this->output->warning('No matching environments found between ymir.yml and vapor.yml files');
+
+            return;
+        }
+
+        $this->getProjectConfiguration()->applyChangesToEnvironments(new VaporConfigurationChange($vaporConfiguration));
+
+        $imageDeploymentEnvironmentConfigurations = $this->getImageDeploymentEnvironmentConfigurations($matchedEnvironments);
+
+        if (!$imageDeploymentEnvironmentConfigurations->isEmpty()) {
+            $this->migrateDockerfiles($imageDeploymentEnvironmentConfigurations);
+        }
+
+        $this->output->info('Vapor configuration migrated into <comment>ymir.yml</comment> file for the following environment(s):');
+        $this->output->list($matchedEnvironments);
+    }
+
+    /**
+     * Back up all relevant Dockerfiles before migration.
+     */
+    private function backupDockerfiles(Collection $environmentConfigurations, bool $backupGlobalDockerfile): void
+    {
+        $dockerfilePaths = $environmentConfigurations
+            ->map(function (EnvironmentConfiguration $environmentConfiguration): string {
+                return $this->generateDockerfilePath($environmentConfiguration->getName());
+            })
+            ->unique();
+
+        if ($backupGlobalDockerfile) {
+            $dockerfilePaths->push($this->generateDockerfilePath());
+        }
+
+        $dockerfilePaths->filter(function (string $dockerfilePath): bool {
+            return $this->filesystem->exists($dockerfilePath);
+        })->each(function (string $dockerfilePath): void {
+            $this->filesystem->rename($dockerfilePath, $dockerfilePath.'.bak', true);
+        });
+    }
+
+    /**
+     * Create a Dockerfile.
+     */
+    private function createDockerfile(EnvironmentConfiguration $environmentConfiguration, string $phpVersion, bool $globalDockerfile): void
+    {
+        $architecture = $environmentConfiguration->getArchitecture() ?: 'x86_64';
+        $environment = $globalDockerfile ? '' : $environmentConfiguration->getName();
+
+        $this->dockerfile->create($architecture, $phpVersion, $environment);
+
+        $this->output->info($this->generateDockerfileCreatedMessage($architecture, $environment, $phpVersion));
+
+        if ($globalDockerfile) {
+            $this->output->comment(sprintf('Using <comment>%s</comment> environment configuration', $environmentConfiguration->getName()));
+        }
+    }
+
+    /**
+     * Generate the success message after creating the Dockerfile.
+     */
+    private function generateDockerfileCreatedMessage(string $architecture, string $environment, string $phpVersion): string
+    {
+        return sprintf('Created <comment>%s</comment> for PHP <comment>%s</comment> and <comment>%s</comment> architecture', Dockerfile::getFileName($environment), $phpVersion, $architecture);
+    }
+
+    /**
+     * Generate the full Dockerfile path.
+     */
+    private function generateDockerfilePath(string $environment = ''): string
+    {
+        return sprintf('%s/%s', $this->getProjectDirectory(), Dockerfile::getFileName($environment));
+    }
+
+    /**
+     * Get the fallback PHP version from environment or project type.
+     */
+    private function getFallbackPhpVersion(EnvironmentConfiguration $environmentConfiguration): string
+    {
+        return empty($environmentConfiguration->getPhpVersion()) ? $this->getProjectConfiguration()->getProjectType()->getDefaultPhpVersion() : $environmentConfiguration->getPhpVersion();
+    }
+
+    /**
+     * Get the image deployment environment configurations to use for Dockerfile migration.
+     */
+    private function getImageDeploymentEnvironmentConfigurations(Collection $matchedEnvironments): Collection
+    {
+        return $matchedEnvironments
+            ->map(function (string $environment): EnvironmentConfiguration {
+                return $this->getProjectConfiguration()->getEnvironmentConfiguration($environment);
+            })
+            ->filter(function (EnvironmentConfiguration $environmentConfiguration): bool {
+                return $environmentConfiguration->isImageDeploymentType();
+            })
+            ->values();
+    }
+
+    /**
+     * Get the parsed Vapor configuration.
+     */
+    private function getVaporConfiguration(): array
+    {
+        $vaporConfigurationFilePath = $this->getProjectDirectory().'/vapor.yml';
+
+        if (!$this->filesystem->exists($vaporConfigurationFilePath)) {
+            throw new InvalidInputException(sprintf('No vapor configuration file found at "%s"', $vaporConfigurationFilePath));
+        }
+
+        try {
+            $vaporConfiguration = Yaml::parse((string) file_get_contents($vaporConfigurationFilePath));
+        } catch (\Throwable $exception) {
+            throw new InvalidInputException(sprintf('Error parsing Vapor configuration file: %s', $exception->getMessage()));
+        }
+
+        if (!is_array($vaporConfiguration)) {
+            throw new InvalidInputException('Error parsing Vapor configuration file');
+        }
+
+        return $vaporConfiguration;
+    }
+
+    /**
+     * Migrate Dockerfiles for image deployment environments.
+     */
+    private function migrateDockerfiles(Collection $environmentConfigurations): void
+    {
+        $phpVersions = $environmentConfigurations->mapWithKeys(function (EnvironmentConfiguration $environmentConfiguration): array {
+            return [$environmentConfiguration->getName() => $this->resolvePhpVersion($environmentConfiguration, $this->generateDockerfilePath($environmentConfiguration->getName()))];
+        });
+        $createGlobalDockerfile = $this->output->confirm('Do you want to create one global <comment>Dockerfile</comment> for all image deployment environments?', false);
+        $sourceEnvironmentConfiguration = $this->selectDockerfileSourceEnvironmentConfiguration($environmentConfigurations);
+        $dockerfileConfigurations = $createGlobalDockerfile ? collect([$sourceEnvironmentConfiguration]) : $environmentConfigurations;
+
+        $this->backupDockerfiles($environmentConfigurations, $createGlobalDockerfile);
+
+        $dockerfileConfigurations->each(function (EnvironmentConfiguration $environmentConfiguration) use ($createGlobalDockerfile, $phpVersions): void {
+            $this->createDockerfile($environmentConfiguration, (string) $phpVersions->get($environmentConfiguration->getName()), $createGlobalDockerfile);
+        });
+    }
+
+    /**
+     * Resolve the PHP version from the existing Dockerfile content.
+     */
+    private function resolvePhpVersion(EnvironmentConfiguration $environmentConfiguration, string $dockerfilePath): string
+    {
+        if (!$this->filesystem->exists($dockerfilePath)) {
+            return $this->getFallbackPhpVersion($environmentConfiguration);
+        }
+
+        $dockerfileContent = (string) file_get_contents($dockerfilePath);
+
+        if (1 !== preg_match('/:php-?(?:(\d+)\.(\d+)|(\d{2,3}))/i', $dockerfileContent, $matches)) {
+            return $this->getFallbackPhpVersion($environmentConfiguration);
+        } elseif (!empty($matches[1]) && !empty($matches[2])) {
+            return sprintf('%s.%s', $matches[1], $matches[2]);
+        } elseif (empty($matches[3])) {
+            return $this->getFallbackPhpVersion($environmentConfiguration);
+        }
+
+        $version = $matches[3];
+
+        return 2 === strlen($version) ? sprintf('%s.%s', $version[0], $version[1]) : sprintf('%s.%s', $version[0], substr($version, 1));
+    }
+
+    /**
+     * Select the source environment for Dockerfile generation.
+     */
+    private function selectDockerfileSourceEnvironmentConfiguration(Collection $environmentConfigurations): EnvironmentConfiguration
+    {
+        $productionEnvironmentConfiguration = $environmentConfigurations->first(function (EnvironmentConfiguration $environmentConfiguration): bool {
+            return 'production' === $environmentConfiguration->getName();
+        });
+
+        return $productionEnvironmentConfiguration instanceof EnvironmentConfiguration ? $productionEnvironmentConfiguration : $environmentConfigurations->first();
+    }
+}
---
+++ tests/Unit/Project/Configuration/Laravel/VaporConfigurationChangeTest.php
@@ -0,0 +1,381 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ymir command-line tool.
+ *
+ * (c) Carl Alexander <support@ymirapp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ymir\Cli\Tests\Unit\Project\Configuration\Laravel;
+
+use Ymir\Cli\Project\Configuration\Laravel\VaporConfigurationChange;
+use Ymir\Cli\Project\EnvironmentConfiguration;
+use Ymir\Cli\Project\Type\ProjectTypeInterface;
+use Ymir\Cli\Tests\TestCase;
+
+class VaporConfigurationChangeTest extends TestCase
+{
+    public static function provideDirectMappings(): array
+    {
+        return [
+            ['build', ['npm ci'], ['build' => ['commands' => ['npm ci']]]],
+            ['cli-timeout', 120, ['console' => ['timeout' => 120]]],
+            ['database', 'server-name', ['database' => ['server' => 'server-name']]],
+            ['database-user', 'database-user', ['database' => ['user' => 'database-user']]],
+            ['domain', 'example.com', ['domain' => 'example.com']],
+            ['firewall.bot-control', true, ['firewall' => ['bots' => true]]],
+            ['firewall.rate-limit', 100, ['firewall' => ['rate_limit' => 100]]],
+            ['memory', 1024, ['memory' => 1024]],
+            ['warm', 5, ['warmup' => 5]],
+            ['concurrency', 25, ['website' => ['concurrency' => 25]]],
+            ['timeout', 30, ['website' => ['timeout' => 30]]],
+        ];
+    }
+
+    public function testApplyAddsDeploymentCommandsAndPreservesNestedDeploymentType(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'deploy' => ['php artisan migrate --force'],
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', [
+            'deployment' => [
+                'type' => 'deployment-type',
+            ],
+        ]);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'deployment' => [
+                'type' => 'deployment-type',
+                'commands' => ['php artisan migrate --force'],
+            ],
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyAddsDeploymentCommandsAndPreservesRootDeploymentType(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'deploy' => ['php artisan migrate --force'],
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', ['deployment' => 'deployment-type']);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'deployment' => [
+                'type' => 'deployment-type',
+                'commands' => ['php artisan migrate --force'],
+            ],
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyAddsDeploymentCommandsWithoutDeploymentType(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'deploy' => ['php artisan migrate --force'],
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging');
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'deployment' => [
+                'commands' => ['php artisan migrate --force'],
+            ],
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyDoesNotChangeDeploymentWhenDeployCommandsAreEmpty(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'deploy' => [],
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', ['deployment' => 'deployment-type']);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'deployment' => 'deployment-type',
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyDoesNothingWhenDeployCommandsAreEmpty(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'deploy' => [],
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', [
+            'deployment' => [
+                'type' => 'deployment-type',
+            ],
+        ]);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'deployment' => [
+                'type' => 'deployment-type',
+            ],
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyDoesNothingWhenEnvironmentNodeIsNotAnArray(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => 'invalid',
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', ['foo' => 'bar']);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'foo' => 'bar',
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyDoesNotOverrideDirectMappingWhenValueIsNull(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'domain' => null,
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', ['domain' => 'existing-domain.com']);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'domain' => 'existing-domain.com',
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyDoesNotSetPhpVersionForImageDeployment(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'runtime' => 'php-8.3-arm',
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', [
+            'deployment' => [
+                'type' => 'image',
+            ],
+            'php' => '8.1',
+        ]);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'deployment' => [
+                'type' => 'image',
+            ],
+            'php' => '8.1',
+            'architecture' => 'arm64',
+        ], $environmentConfiguration->toArray());
+    }
+
+    /**
+     * @dataProvider provideDirectMappings
+     */
+    public function testApplyMapsDirectConfigurationValues(string $vaporKey, $value, array $expected): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    $vaporKey => $value,
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging');
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame($expected, $environmentConfiguration->toArray());
+    }
+
+    public function testApplyMergesQueueDefaultsIntoNamedQueues(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'queue-concurrency' => '5',
+                    'queue-memory' => 512,
+                    'queue-timeout' => 60,
+                    'queues' => [
+                        'emails',
+                        'invoices.fifo' => 3,
+                    ],
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging');
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'queues' => [
+                'emails' => [
+                    'concurrency' => 5,
+                    'memory' => 512,
+                    'timeout' => 60,
+                ],
+                'invoices' => [
+                    'concurrency' => 3,
+                    'memory' => 512,
+                    'timeout' => 60,
+                    'type' => 'fifo',
+                ],
+            ],
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyParsesSingleQueueObjectSyntax(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'queues' => [
+                        ['reports' => 4],
+                    ],
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging');
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'queues' => [
+                'reports' => [
+                    'concurrency' => 4,
+                ],
+            ],
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplyRemovesQueuesWhenVaporQueuesDisabled(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'queues' => false,
+                    'queue-concurrency' => 5,
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', [
+            'memory' => 512,
+            'queues' => [
+                'default' => ['concurrency' => 10],
+            ],
+        ]);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'memory' => 512,
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplySetsArchitectureAndPhpFromRuntimeWhenNotImageDeployment(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'runtime' => 'php-8.2-arm',
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', [
+            'deployment' => [
+                'type' => 'deployment-type',
+            ],
+        ]);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'deployment' => [
+                'type' => 'deployment-type',
+            ],
+            'architecture' => 'arm64',
+            'php' => '8.2',
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplySetsCronToFalseWhenSchedulerDisabled(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'scheduler' => false,
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging', [
+            'cron' => true,
+        ]);
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'cron' => false,
+        ], $environmentConfiguration->toArray());
+    }
+
+    public function testApplySetsQueueDefaultsWhenOnlyQueueDefaultsAreConfigured(): void
+    {
+        $change = new VaporConfigurationChange([
+            'environments' => [
+                'staging' => [
+                    'queue-concurrency' => '5',
+                    'queue-memory' => 512,
+                    'queue-timeout' => 60,
+                ],
+            ],
+        ]);
+        $environmentConfiguration = new EnvironmentConfiguration('staging');
+
+        $environmentConfiguration = $change->apply($environmentConfiguration, \Mockery::mock(ProjectTypeInterface::class));
+
+        $this->assertSame([
+            'queues' => [
+                'concurrency' => 5,
+                'memory' => 512,
+                'timeout' => 60,
+            ],
+        ], $environmentConfiguration->toArray());
+    }
+}
